d03328f8e4b90ada1f339500c76c71e44ed91513
Candehaedo11 <candehaedo64@gmail.com>
2024-08-31 15:10:30 -0300

    correción

--- Todos los archivos/archivos js/Brune juegos.js
+++ Todos los archivos/archivos js/Brune juegos.js
@@ -5,7 +5,7 @@ document.getElementById("btn-1-1").addEventListener("click", function() {
   document.getElementById("resultado1").innerText = "Incorrecto";
 });
 document.getElementById("btn-1-2").addEventListener("click", function() {
-  document.getElementById("resultado1").innerText = "Correcto";
+  document.getElementById("resultado1").innerText = "Correcto!";
 });
 document.getElementById("btn-1-3").addEventListener("click", function() {
   document.getElementById("resultado1").innerText = "Incorrecto";
@@ -19,7 +19,7 @@ document.getElementById("btn-2-2").addEventListener("click", function() {
   document.getElementById("resultado2").innerText = "Incorrecto";
 });
 document.getElementById("btn-2-3").addEventListener("click", function() {
-  document.getElementById("resultado2").innerText = "Correcto";
+  document.getElementById("resultado2").innerText = "Correcto!";
 });
 
 // Pregunta 3
@@ -30,7 +30,7 @@ document.getElementById("btn-3-2").addEventListener("click", function() {
   document.getElementById("resultado3").innerText = "Incorrecto";
 });
 document.getElementById("btn-3-3").addEventListener("click", function() {
-  document.getElementById("resultado3").innerText = "Correcto";
+  document.getElementById("resultado3").innerText = "Correcto!";
 });
 
 // Pregunta 4
@@ -38,7 +38,7 @@ document.getElementById("btn-4-1").addEventListener("click", function() {
   document.getElementById("resultado4").innerText = "Incorrecto";
 });
 document.getElementById("btn-4-2").addEventListener("click", function() {
-  document.getElementById("resultado4").innerText = "Correcto";
+  document.getElementById("resultado4").innerText = "Correcto!";
 });
 document.getElementById("btn-4-3").addEventListener("click", function() {
   document.getElementById("resultado4").innerText = "Incorrecto";
